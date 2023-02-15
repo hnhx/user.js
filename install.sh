cf37b85afe2fc071b9b5d033846bb9e20dfa9b91
hnhx <femboy@tutanota.de>
2023-02-15 08:30:44 +0100

    now ublock gets installed from the official github, changed nohup to setsid

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ rm -rf $HOME/.mozilla/firefox/*.default-release/*
 cp firefox/* $HOME/.mozilla/firefox/*.default-release
 
 ublock_version="1.46.0"
-wget -O ublock_origin-$ublock_version.xpi https://addons.mozilla.org/firefox/downloads/file/4047353/ublock_origin-$ublock_version.xpi
-nohup firefox ublock_origin-$ublock_version.xpi &
+wget -O uBlock0_$ublock_version.firefox.signed.xpi https://github.com/gorhill/uBlock/releases/download/$ublock_version/uBlock0_$ublock_version.firefox.signed.xpi
+setsid -f firefox uBlock0_$ublock_version.firefox.signed.xpi
