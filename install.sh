e90f87b028e469a1f09fd6fa101d99ec4ddbfefc
Amber <kj@vnetwork.com>
2023-05-27 22:13:16 -0500

    fixed userChrome.css copy

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 pkill firefox
 rm -rf $HOME/.mozilla/firefox/*.default-release/*
-cp firefox/* $HOME/.mozilla/firefox/*.default-release
+cp -r firefox/* $HOME/.mozilla/firefox/*.default-release
 
 ublock_version="1.46.0"
 wget -O uBlock0_$ublock_version.firefox.signed.xpi https://github.com/gorhill/uBlock/releases/download/$ublock_version/uBlock0_$ublock_version.firefox.signed.xpi
